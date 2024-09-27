e7d897a46400d2c23dd2595281dc89af1f41b733
Allanah Leyden <allanahleyden@Allanahs-MacBook-Air-2.local>
2024-09-28 01:02:09 +1000

    Configure app for deployment: Update app.py, add Procfile, include gunicorn in requirements.txt

--- app.py
+++ app.py
@@ -155,4 +155,5 @@ def restart():
     return redirect(url_for('select_category'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
